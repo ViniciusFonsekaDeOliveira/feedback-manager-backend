62eee99584b225bedb8015eb6148ca8f7face1c5
Vinícius Fonseca de Oliveira <viniciusfonsekadeoliveira@gmail.com>
2023-09-19 01:29:59 -0300

    Initial commit

---
+++ src/main/java/br/com/ada/FeedbackManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.ada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackManagerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeedbackManagerBackendApplication.class, args);
+    }
+
+}
